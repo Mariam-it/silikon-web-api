88ec3919d8715cfe1ce94d46e42a94f62ddc3560
Mariam <mariam.al.it@hotmail.com>
2024-04-21 23:47:22 +0200

    created web api app

---
+++ Infrastructure/Entities/CourseEntity.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities;
+
+public class CourseEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public bool IsBestSeller { get; set; }
+    public string Image { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string Price { get; set; } = null!;
+    public string? DiscountPrice { get; set; }
+    public string Hours { get; set; } = null!;
+    public string LikesProcent { get; set; } = null!;
+    public string LikesInNumber { get; set; } = null!;
+
+    public int? CategoryId { get; set; }
+    public CategoryEntity? Category { get; set; }
+}
---
+++ Infrastructure/Models/CourseViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Infrastructure.Models;
+
+public class CourseViewModel
+{
+    public int Id { get; set; }
+    public bool IsBestSeller { get; set; }
+    public string Image { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string Price { get; set; } = null!;
+    public string? DiscountPrice { get; set; }
+    public string Hours { get; set; } = null!;
+    public string? LikesProcent { get; set; }
+    public string? LikesInNumber { get; set; }
+    public string Category { get; set; } = null!;
+}
